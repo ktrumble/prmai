f4bccd93569d7e220c0ac0b46a5132a6b38e6c3e
Keith Trumble <keith@generalui.com>
2014-05-14 11:20:13 -0700

    reverting: local works, need to troubleshoot issue on Heroku

--- app/posts.py
+++ app/posts.py
@@ -349,8 +349,8 @@ class ListPosts(app.basic.BaseHandler):
     if is_edit:
       self.redirect('/posts/%s?msg=updated' % post['slug'])
     else:
-      self.redirect('/posts/%s' % post['slug'])
-#      self.redirect('/?msg=success&slug=%s' % post['slug'])
+#      self.redirect('/posts/%s' % post['slug'])
+      self.redirect('/?msg=success&slug=%s' % post['slug'])
 
 
 ##############
